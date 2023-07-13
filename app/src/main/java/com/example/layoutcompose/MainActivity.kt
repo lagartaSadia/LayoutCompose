04c63af4adea58eb8331301946da7c5414c3a256
lagartaSadia <campolino.l@gmail.com>
2023-07-13 01:37:56 -0300

    Couldn't recreated exactly the same layout on my own, but reading the solution I figure it out.

--- app/src/main/java/com/example/layoutcompose/MainActivity.kt
+++ app/src/main/java/com/example/layoutcompose/MainActivity.kt
@@ -3,16 +3,24 @@ package com.example.layoutcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.layoutcompose.ui.theme.LayoutComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,9 +30,9 @@ class MainActivity : ComponentActivity() {
             LayoutComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize()
                 ) {
+
                 }
             }
         }
@@ -32,12 +40,58 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CardsGreetingWithText(title: String, description: String) {
+fun ComposeLayoutApp() {
+    Column(Modifier.fillMaxWidth()) {
+        Row(Modifier.weight(1f)) {
+            ComposeInfoCard(
+                title = stringResource(R.string.first_title),
+                description = stringResource(R.string.first_description),
+                backgroundColor = Color.Green,
+                modifier = Modifier.weight(1f)
+            )
+            ComposeInfoCard(
+                title = stringResource(R.string.second_title),
+                description = stringResource(R.string.second_description),
+                backgroundColor = Color.Yellow,
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(Modifier.weight(1f)) {
+            ComposeInfoCard(
+                title = stringResource(R.string.third_title),
+                description = stringResource(R.string.third_description),
+                backgroundColor = Color.Cyan,
+                modifier = Modifier.weight(1f)
+            )
+            ComposeInfoCard(
+                title = stringResource(R.string.fourth_title),
+                description = stringResource(R.string.fourth_description),
+                backgroundColor = Color.LightGray,
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
+
+@Composable
+fun ComposeInfoCard(
+    title: String,
+    description: String,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+) {
     Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = title
+            text = title,
+            modifier = Modifier.padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold
         )
         Text(
             text = description,
@@ -46,15 +100,9 @@ fun CardsGreetingWithText(title: String, description: String) {
     }
 }
 
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    LayoutComposeTheme {
-        Column {
-            CardsGreetingWithText("Text composable", "Displays text and follows Material Design guidelines.")
-            CardsGreetingWithText("Image composable", "Creates a composable that lays out and draws a given Painter class object.")
-            CardsGreetingWithText("Row composable", "A layout composable that places its children in a horizontal sequence.")
-            CardsGreetingWithText("Column composable", "A layout composable that places its children in a vertical sequence.")
-        }
-    }
+    ComposeLayoutApp()
 }
