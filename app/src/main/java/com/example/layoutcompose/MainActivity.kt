f769879339a2f29cb37947a51cf9b61d17aca15e
lagartaSadia <campolino.l@gmail.com>
2023-07-12 23:02:30 -0300

    Cards text done, but need change because can't color separated cards.

--- app/src/main/java/com/example/layoutcompose/MainActivity.kt
+++ app/src/main/java/com/example/layoutcompose/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.layoutcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.layoutcompose.ui.theme.LayoutComposeTheme
 
@@ -22,7 +25,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
                 }
             }
         }
@@ -30,17 +32,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CardsGreetingWithText(title: String, description: String) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = title
+        )
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     LayoutComposeTheme {
-        Greeting("Android")
+        Column {
+            CardsGreetingWithText("Text composable", "Displays text and follows Material Design guidelines.")
+            CardsGreetingWithText("Image composable", "Creates a composable that lays out and draws a given Painter class object.")
+            CardsGreetingWithText("Row composable", "A layout composable that places its children in a horizontal sequence.")
+            CardsGreetingWithText("Column composable", "A layout composable that places its children in a vertical sequence.")
+        }
     }
 }
